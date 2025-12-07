080c8e4d7d8af393e8c1af0ab35b4eca8f2de6a2
Edward van Tonder <56725833+EA-Wardie@users.noreply.github.com>
2025-12-07 20:11:25 +0200

    Network status && minor style fix for mobile scroll overflow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 
 	let count: number = $state(0);
 	let notificationPermission: NotificationPermission = $state('default');
+	let networkStatus: 'online' | 'offline' = $state('online');
 
 	const promptForNotificationPermissions = async () => {
 		notificationPermission = await Notification.requestPermission();
@@ -10,6 +11,14 @@
 
 	onMount(async () => {
 		await promptForNotificationPermissions();
+
+		addEventListener('online', () => {
+			networkStatus = 'online';
+		});
+
+		addEventListener('offline', () => {
+			networkStatus = 'offline';
+		});
 	});
 
 	const sendTestPushNotificationMessage = async (): Promise<void> => {
@@ -26,11 +35,30 @@
 	};
 </script>
 
-<section style="height: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+<section class="container">
 	<h1>{count}</h1>
 	<button onclick={() => count++}>Add Count</button>
-	<h3>Notifications are: {notificationPermission}</h3>
-	<button disabled="{notificationPermission !== 'granted'}" onclick={sendTestPushNotificationMessage}>
+	<h3>
+		<span>Network Status:</span>
+		<span style="color: {networkStatus === 'online' ? 'green' : 'orange'}">{networkStatus}</span>
+	</h3>
+	<h3>
+		<span>Notification permissions are:</span>
+		<span style="color: {notificationPermission === 'granted' ? 'green' : 'orange'}">{notificationPermission}</span>
+	</h3>
+	<button
+		disabled="{notificationPermission !== 'granted'}"
+		onclick={sendTestPushNotificationMessage}>
 		Send Test Push Notification
 	</button>
 </section>
+
+<style>
+    .container {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
